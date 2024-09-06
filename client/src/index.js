975d6051492d9f0a64d961064d52a507e0607ccc
taha-hme <tahahashemi463@gmail.com>
2024-09-06 23:12:29 +0330

    final

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';  // For basic styling
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
